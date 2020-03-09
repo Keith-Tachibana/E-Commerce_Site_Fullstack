a3c06ec98d4e6d27e289b9098a3170db1f5c3e74
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-03-09 12:43:38 -0700

    Wicked Sales JS: Made images all same height | Keith Tachibana

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -7,7 +7,7 @@ class ProductListItem extends Component {
       return (
         <div key={product.productId} className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 mt-4 mb-4">
           <div className="card h-100">
-            <img className="card-img-top img-fluid" src={product.image} alt={product.name} />
+            <img className="card-img-top img-fluid" src={product.image} alt={product.name} height="50" width="100" />
             <div className="card-body">
               <h3 className="card-title">{product.name}</h3>
               <h6 className="card-text text-secondary">${(product.price / 100).toFixed(2)}</h6>
