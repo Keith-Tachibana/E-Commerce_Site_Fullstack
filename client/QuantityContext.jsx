20f031464d1d47aa371ac1efee55c7c0d8af6180
Keith-Tachibana <Keith.Tachibana@gmail.com>
2021-06-25 06:51:59 -0700

    Added qty context

---
+++ client/QuantityContext.jsx
@@ -0,0 +1,3 @@
+import { createContext } from 'react';
+
+export const QuantityContext = createContext(1);
--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { QuantityContext } from '../QuantityContext';
 
 import Header from './header';
 import ProductList from './product-list';
